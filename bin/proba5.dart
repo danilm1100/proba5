269a678cc42a2ebb6d7bb39c8f4e45c9972a2537
danilm1100 <danilmoiseev56473@gmail.com>
2023-11-09 11:05:25 +0500

    worksleeprepit

---
+++ bin/proba5.dart
@@ -0,0 +1,5 @@
+import 'package:proba5/proba5.dart' as proba5;
+
+void main(List<String> arguments) {
+  print('Hello world: ${proba5.calculate()}!');
+}
---
+++ test/proba5_test.dart
@@ -0,0 +1,8 @@
+import 'package:proba5/proba5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
